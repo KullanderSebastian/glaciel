5f661a3f53dc93f0777343ef0b97ac6dbb6868ae
KullanderSebastian <kullander.sebastian@gmail.com>
2024-11-28 19:01:47 +0100

    chore: initial commit

---
+++ src/app/layout/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="bg-gray-100 h-[52px] border-b-rose-700 border-b">
+            <h1>Gla ciel</h1>
+            <div>
+                <p>Home</p>
+                <p>Products</p>
+                <p>About</p>
+                <p>Contact</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
